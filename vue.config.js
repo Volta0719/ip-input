9baf5c9963216d4e1c5eb938102fd41edab4630b
ltinerary <948615201@qq.com>
2021-12-12 18:50:51 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,14 @@
  * @Author: 24min
  * @Date: 2021-12-05 13:13:04
  * @LastEditors: 24min
- * @LastEditTime: 2021-12-12 18:16:02
+ * @LastEditTime: 2021-12-12 18:48:26
  * @FilePath: \ip-input\vue.config.js
  * @note: If it ain't broke, don't fix it.🍤
  * @Description: to bo continued...
  */
 module.exports = {
     productionSourceMap: false, //close production sourceMap
+    publicPath: process.env.NODE_ENV === "development" ? "" : "/ip-input/dist/",
     css: {
         extract: false
     },
