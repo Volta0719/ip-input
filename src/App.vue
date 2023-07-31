1bda380ba30853b701b29ef4fc7ed2db603c5ed9
meizu <fan24min@qq.com>
2023-07-31 09:28:57 +0800

    🚖 修复node版本过高命令运行问题 修改在线链接中 url地址指向问题

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: 24min
  * @Date: 2021-12-04 13:12:44
- * @LastEditors: 24min
- * @LastEditTime: 2021-12-12 19:07:55
+ * @LastEditors: fanjf
+ * @LastEditTime: 2023-07-31 09:24:34
  * @FilePath: \ip-input\src\App.vue
  * @note: If it ain't broke, don't fix it.🍤
  * @Description: to bo continued...
@@ -11,7 +11,7 @@
   <div id="app">
     <h1>ip输入框在线体验</h1>
     <fan-ip-input></fan-ip-input>
-    <h1>https://github.com/24min/ip-input</h1>
+    <h1>https://github.com/Volta0719/ip-input</h1>
   </div>
 </template>
 
