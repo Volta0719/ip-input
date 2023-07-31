446508b703d51875f3a4d11c57db1802f12618e7
meizu <fan24min@qq.com>
2023-07-31 10:16:02 +0800

    🥕 粘贴功能

--- src/components/ipInput.vue
+++ src/components/ipInput.vue
@@ -2,28 +2,19 @@
  * @Author: 24min
  * @Date: 2021-12-04 20:06:28
  * @LastEditors: fanjf
- * @LastEditTime: 2023-07-31 09:32:17
+ * @LastEditTime: 2023-07-31 10:15:14
  * @FilePath: \ip-input\src\components\ipInput.vue
  * @note: If it ain't broke, don't fix it.🍤
  * @Description: to bo continued...
 -->
 <template>
-  <ul class="fan-ip-addr">
+  <ul class="fan-ip-addr" @paste="pasteIP($event)">
     <li v-for="(item, index) in ip" :key="index" class="fan-ip-item">
-      <a-input
-        size="small"
-        v-model.number="item.value"
-        class="fan-ip-input"
-        ref="ipInput"
-        :maxLength="3"
-        @focus.native="focusInput($event, index)"
-        @blur.native="blurInput($event, index)"
-        @keyup.native="pressKey($event, index, item)"
-        @keydown.native="keydown($event, index)"
-        @compositionstart="compositionstart($event, index)"
-        @compositionend="compositionend($event, index)"
-        @change="changeIp($event, index)"
-      ></a-input>
+      <a-input size="small" v-model.number="item.value" class="fan-ip-input" ref="ipInput" :maxLength="3"
+        @focus.native="focusInput($event, index)" @blur.native="blurInput($event, index)"
+        @keyup.native="pressKey($event, index, item)" @keydown.native="keydown($event, index)"
+        @compositionstart="compositionstart($event, index)" @compositionend="compositionend($event, index)"
+        @change="changeIp($event, index)"></a-input>
       <span class="fan-ip-dot" v-if="index < 3"></span>
     </li>
   </ul>
@@ -31,6 +22,7 @@
 
 <script>
 import { Input } from "ant-design-vue";
+const ipRegex = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
 export default {
   name: "fanIpInput",
   data() {
@@ -75,6 +67,18 @@ export default {
     },
   },
   methods: {
+    pasteIP(e) {
+      const pasteData = e.clipboardData.getData("Text");
+      const resultIP = pasteData.match(ipRegex);
+      if (!!resultIP) {
+        this.ip = [];
+        resultIP[0].split(".").forEach((ele => {
+          this.ip.push({ value: ele });
+        }))
+      } else {
+        console.log(`[fan-ip-input]we did't found an ip in your paste Data("${pasteData}") `)
+      }
+    },
     changeIp(e, index) {
       // console.log("changeIp", e);
       if (this.shouldRemoveText) {
@@ -93,7 +97,7 @@ export default {
             currentEvent.selectionEnd = this.beforePosition;
           });
         } else {
-          console.error(`we didn't match the text[${this.shouldRemoveText}] in ${index} value😅`);
+          console.error(`[fan-ip-input]we didn't match the text[${this.shouldRemoveText}] in ${index} value😅`);
         }
       } else {
         if (e.currentTarget.selectionStart === 3) {
@@ -162,8 +166,6 @@ export default {
       }
     },
     blurInput(e, index) {
-      //   const value
-      // console.log("blurInput", e);
       const { value } = e.currentTarget;
       if (+value > 255) {
         this.ip[index].value = 255;
@@ -190,7 +192,8 @@ export default {
         "0",
       ];
       this.prenentKeycode = e.key === "Process" ? e.code : "";
-      if (!allowKey.includes(e.key)) {
+      //将ctrl-v 放行
+      if (!allowKey.includes(e.key) && !(e.code === 'KeyV' && e.ctrlKey)) {
         e.preventDefault();
       }
     },
@@ -287,8 +290,10 @@ export default {
   justify-content: space-around;
   width: 190px;
   height: 32px;
+
   & .fan-ip-item {
     line-height: 32px;
+
     & .fan-ip-dot {
       display: inline-block;
       width: 2px;
@@ -297,11 +302,13 @@ export default {
       border-radius: 50%;
       box-shadow: 0 0 0 1px #fff;
     }
+
     & .fan-ip-input {
       border: none;
       width: 40px;
       position: relative;
       padding: 3px 8px;
+
       &:focus {
         box-shadow: none;
       }
